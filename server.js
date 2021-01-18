0487df41a1eb143690bdaa053aeb6948e0a7f9b0
mandarinkb <mandarinkb@gmail.com>
2021-01-18 22:40:09 +0700

    show images from api

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express')
+const path = require('path'); 
 const response = require('./configs/response') // import ค่ามาจาก response.js
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send(response.firstResponse)
-})
+// app.get('/', (req, res) => {
+//   res.send(response.firstResponse)
+// })
 
-// port
-app.listen(3000, () => {
-  console.log('Start server at port 3000.')
-})
+// // port thinkpad-wallpaper.jpg
+// app.listen(3000, () => {
+//   console.log('Start server at port 3000.')
+// })
+
+// get path from my project ==> __dirname
+// get path from my computer
+app.use(express.static(path.join('/home', 'mandarin')));
+app.listen(3500, function () {
+    console.log('Express server is listening, use this url - localhost:3500/icon.jpg');
+});
